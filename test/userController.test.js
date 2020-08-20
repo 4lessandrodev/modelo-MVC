c621c26d7c225d1f49a3c5a1fcda2302a62f517e
Alessandro <alessandroadm@live.com>
2020-08-20 00:51:09 -0300

    doc(exemple): adding exemples for jest

--- test/userController.test.js
+++ test/userController.test.js
@@ -1,3 +1,5 @@
+//https://jestjs.io/docs/en/using-matchers
+
 const userController = require("../controllers/userController");
 
 
@@ -6,6 +8,64 @@ const userController = require("../controllers/userController");
 // beforeAll(() =>  console.log("Serei executado antes de todos os testes"));
 // afterAll(() =>  console.log("Serei executado depois de todos os testes"));
 
+// Tipos de comparação com jest 
+/*
+
+objetos 
+expect(data).toEqual({one: 1, two: 2});
+
+negação
+expect(a + b).not.toBe(0);
+
+nulo
+expect(n).toBeNull();
+
+definidos
+expect(n).toBeDefined();
+
+não definidos undefinide
+expect(n).not.toBeUndefined();
+
+não ser verdadeiro
+expect(n).not.toBeTruthy();
+
+falso
+expect(n).toBeFalsy();
+
+nulo
+expect(z).not.toBeNull();
+
+definido
+expect(z).toBeDefined();
+
+maior que
+expect(value).toBeGreaterThan(3);
+
+maior ou igual 
+expect(value).toBeGreaterThanOrEqual(3.5);
+
+menor que 
+expect(value).toBeLessThan(5);
+
+menor que ou igual 
+expect(value).toBeLessThanOrEqual(4.5);
+
+deve ser 
+expect(value).toBe(4);
+
+deve ser igual > números e objetos 
+expect(value).toEqual(4);
+
+deve dar match > regex
+expect('Christoph').toMatch(/stop/);
+
+é esperado um erro 
+expect(value).toThrow('you are using the wrong JDK');
+
+é esperado um erro que contenha a palavra JDK
+expect(value).toThrow(/JDK/);
+
+*/
 
 //Nome do grupo de teste
 describe('Validando salvar usuário ', () => {
